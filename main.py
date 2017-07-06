300333a028caf78cb7689432356de8b8a0b1ac65
Kevin Tam <tam.kevin@hotmail.com>
2017-07-06 09:48:00 -0700

    add comments to make it more readable

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+# Creates a database called blog
 class Blog(db.Model):
-    
+    # Creates the table
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
@@ -17,6 +20,7 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+# Displays the blog page
 @app.route('/', methods=['POST', 'GET'])
 def post():
         return render_template('blog.html')
